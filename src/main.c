8d2b7a30840bba9dd409d8081c167c355b88d86c
Persian Prince <persianpros@yahoo.com>
2019-12-17 14:24:52 +0330

    Update main.c

--- src/main.c
+++ src/main.c
@@ -22,7 +22,7 @@ typedef struct {
     MainFunctionHandler_t handler;
 } Entry_t;
 
-Entry_t g_applets[] = {{"lsdir", lsdir_main}, {"nice", nice_main}};
+Entry_t g_applets[] = {"lsdir", lsdir_main};
 
 int main(int argc, char **argv)
 {
